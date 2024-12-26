0cd59be14b9c1158a5ca156b4dec16ec19255f07
marsyg <net.maaz@yahoo.com>
2024-12-26 17:06:06 +0530

    basic snowflake configuration

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+var snowflake = require('snowflake-sdk');
+var dotenv = require('dotenv');
+dotenv.config();
+var connection = snowflake.createConnection({
+    account: process.env.SNOWFLAKE_ACCOUNT,
+    username: process.env.SNOWFLAKE_USER,
+    password: process.env.SNOWFLAKE_PASSWORD,
+    application: 'application'
+  });
+  connection.connect( 
+    function(err, conn) {
+        if (err) {
+            console.error('Unable to connect: ' + err.message);
+            } 
+        else {
+            console.log('Successfully connected to Snowflake.');
+            // Optional: store the connection ID.
+            connection_ID = conn.getId();
+            }
+    }
+);
